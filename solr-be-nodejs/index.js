e6d39c7ab188f5fb2f43f81dfc257336a07e1dda
DESKTOP-BK62AB2\hp Zbook <tuan.vuhoang.bk@gmail.com>
2022-01-05 23:22:29 +0700

    update search with filter document

--- solr-be-nodejs/index.js
+++ solr-be-nodejs/index.js
@@ -9,18 +9,90 @@ app.get("/", (req, res) => {
 var client = new SolrNode({
     host: 'localhost',
     port: '8983',
-    core: 'imdbdata',
+    core: 'db2',
     protocol: 'http'
 })
 //---------log------------
-require('log4js').getLogger("solr-node").level = "DEBUG";
+// require('log4js').getLogger("solr-node").level = "DEBUG";
 //---------crawler------------
 //----------api seach-----------
-app.get("/getall", async (req, res) => {
+//-----------fectch data with pagination-----------
+app.get("/get", async (req, res) => {
     try {
-        var objQuery = client.query().q().start(0).rows(10);
+        var start = req.query.start;
+        if (!start) start = 0;
+        var rows = req.query.rows;
+        if (!rows) rows = 10;
+        var objQuery = client.query().q().start(start).rows(rows);
         const result = await client.search(objQuery);
         res.send(result);
+    } catch (error) {
+        console.log(error)
+        res.status(400).send(error)
+    }
+})
+//-----------select fields to fetch-----------
+app.get("/get/fields", async (req, res) => {
+    try {
+        // default rows and start
+        var start = req.query.start;
+        if (!start) start = 0;
+        var rows = req.query.rows;
+        if (!rows) rows = 10;
+        var q = req.query.q
+        if (!q) q = "*"
+        var rawStringQuery = `q=${q}&fl=${req.query.fl}&start=${start}&rows=${rows}&wt=json`
+        const result = await client.search(rawStringQuery);
+        res.send(result);
+    } catch (error) {
+        console.log(error)
+        res.status(400).send(error)
+    }
+})
+//-----------filter document to fetch-----------
+app.get("/get/filter", async (req, res) => {
+    try {
+        var q = req.query
+        // default rows and start
+        var start = q.start;
+        if (!start) start = 0;
+        var rows = q.rows;
+        if (!rows) rows = 10;
+        var fl
+        if (q.fields) {
+            fl = q.fields
+        } else {
+            fields = "*"
+        }
+        delete q.fields
+        var raw = objToString(q);
+        var rawQuery = `q=${raw}&fl=${fl}&wt=json`
+        const result = await client.search(rawQuery);
+        res.send(result)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send(error)
+    }
+})
+app.get("/test", async (req, res) => {
+    try {
+        var q = req.query
+        // default rows and start
+        var start = q.start;
+        if (!start) start = 0;
+        var rows = q.rows;
+        if (!rows) rows = 10;
+        var fl
+        if (q.fields) {
+            fl = q.fields
+        } else {
+            fields = "*"
+        }
+        delete q.fields
+        var raw = objToString(q);
+        var rawQuery = `q=${raw}&fl=${fl}&wt=json`
+        const result = await client.search(rawQuery);
+        res.send(result)
     } catch (error) {
         console.log(error)
         res.status(400).send(error)
@@ -30,3 +102,19 @@ app.get("/getall", async (req, res) => {
 app.listen(3000, () => {
     console.log("App is running on port 3000");
 })
+
+/**
+ * 
+ * @param {Object} obj 
+ * @returns String
+ */
+function objToString(obj) {
+    var tmp = obj;
+    var stringified = JSON.stringify(tmp);
+
+    stringified = stringified.replace(/"/g, "")
+    stringified = stringified.replace("{", "")
+    stringified = stringified.replace("}", "")
+
+    return stringified;
+}
---
+++ solr-be-nodejs/models/film.model.js
@@ -0,0 +1,23 @@
+class Film {
+    type_
+    url
+    name //tên bộ phim,
+    alternateName //????,
+    image //link ảnh đại diện,
+    description //Mô tả phim,
+    aggregateRating_ratingCount //số lượt đánh giá,
+    aggregateRating_bestRating //điểm cao nhất đạt dc,
+    aggregateRating_worstRating //điểm thấp nhất đạt dc,
+    aggregateRating_ratingValue //trung bình đánh giá,
+    genre //
+    datePublished //Ngày ra mắt,
+    keywords //từ khóa,
+    trailer_name //Tên trailer,
+    trailer_embedUrl //Đường dẫn nhúng,
+    trailer_thumbnailUrl //ảnh trailer,
+    trailer_description //mô tả trailer,
+    actors //,
+    directors //
+    creators // array 
+    duration // thời lượng
+}
